3277068699f949adc2071db27357f020204b49a3
yongzhi <qiyongzhi2007@qq.com>
2017-07-29 00:13:38 -0700

    Add files via upload

---
+++ static/draw-graph.js
@@ -0,0 +1,402 @@
+$(window).load(function () {
+	console.log('jojojoojojjo')	
+	// initialize(state);	
+});
+
+var width = 800,
+    height = 600,
+    margin = 30;
+var JUNCTION = 0,
+    CONSUMER = 1,
+    SOURCE = 2,
+    TANK = 3;
+var PIPE = 0,
+    PUMP = 1,
+    VALVE = 2;
+var radius = 3;
+
+var nodes, edges;
+var pressureScale, flowScale;
+var xScale, yScale;
+
+d3.selection.prototype.moveToFront = function() {  
+      return this.each(function(){
+        this.parentNode.appendChild(this);
+      });
+    };
+
+d3.selection.prototype.moveToBack = function() {  
+    return this.each(function() { 
+        var firstChild = this.parentNode.firstChild; 
+        if (firstChild) { 
+            this.parentNode.insertBefore(this, firstChild); 
+        } 
+    });
+};
+
+// ============== Color ============== //
+
+function colorNodeType(d, color) {
+    switch (d.node_type) {
+        case CONSUMER:
+            return "white" //consumer
+        case SOURCE:
+            return "orange" //source
+        case TANK:
+            return "green" //tank
+        default:
+            return color(d.pressure);
+    }
+}
+
+function colorEdgeType(d) {
+    switch (d.edge_type) {
+        case PUMP:
+            return "orange" //pump
+        case VALVE:
+            return "green" //valve
+        default:
+            return "steelblue"; //pipe
+    }
+}
+
+
+// ============== Scale ============== //
+function getFlowScale() {
+    var flows = edges.map(function(o) {
+        return o.flow;
+    });
+    var max_flow = arr.max(flows);
+    var min_flow = arr.min(flows);
+    console.log('fmax', max_flow);
+    console.log('fmin', min_flow);
+    var flowScale = d3.scale.linear()
+        .domain([min_flow, max_flow])
+        .range([1, 30]);
+    return flowScale;
+}
+
+function getPressureScale() {
+    var pressures = nodes.filter(function(o) {
+            return o.node_type != SOURCE;
+        })
+        .map(function(o) {
+            return o.pressure;
+        });
+    var mean = arr.mean(pressures);
+    var sd = arr.standardDeviation(pressures)
+    var color = d3.scale.linear().domain([mean - 2 * sd, mean + 2 * sd])
+        .interpolate(d3.interpolateHcl)
+        .range([d3.rgb("#B5F394"), d3.rgb("#2c7bb6")]);
+    return color;
+}
+
+function getPositionScale() {
+    var x = nodes.map(function(o) {
+        return o.x;
+    });
+    var y = nodes.map(function(o) {
+        return o.y;
+    });
+    var max_x = arr.max(x);
+    var min_x = arr.min(x);
+    var max_y = arr.max(y);
+    var min_y = arr.min(y);
+    xScale = d3.scale.linear()
+        .domain([min_x, max_x])
+        .range([0, width - 2 * margin]);
+    yScale = d3.scale.linear()
+        .domain([min_y, max_y])
+        .range([0, height - 2 * margin]);
+    return {
+        x: xScale,
+        y: yScale
+    };
+}
+
+// ============== Draw ============== //
+function drawNodes(xScale, yScale, color) {
+    d3.select("#graph-canvas").selectAll("circle")
+        .data(nodes).enter().append("circle")
+        .attr("r", 4)
+        .attr("fill", function(d) {
+            return colorNodeType(d, color);
+        })        
+        .attr("cx", function(d) {
+            return margin + xScale(d.x);
+        })
+        .attr("cy", function(d) {
+            return margin + yScale(d.y);
+        })
+        .on("mouseover", function() {
+            d3.select(this).attr("r", 6);
+        })
+        .on("mouseout", function() {
+            d3.select(this).attr("r", 4);
+        });
+
+    d3.select("#graph-canvas").selectAll("circle")
+        .filter(function(d, i) {
+            return d.node_type == CONSUMER;
+        })
+        .attr("stroke", "red")
+        .attr("stroke-width", 2)
+        .attr("stroke-opacity", 0.8);
+
+    $('#graph-canvas circle').tipsy({
+        gravity: 'w',
+        html: true,
+        title: function() {
+            var d = this.__data__;
+            var info = '';
+            info += 'node_id: ' + d.node_id + '<br />';
+            info += 'demand: ' + d.demand.toFixed(2) + '<br />';
+            info += 'height: ' + d.head.toFixed(2) + '<br />';
+            info += 'head: ' + d.pressure.toFixed(2);
+            return info;
+        }
+    });
+}
+
+function drawEdges(xScale, yScale) {
+    d3.select("#graph-canvas").selectAll("line")
+        .data(edges).enter().append("line")
+        .attr("x1", function(d) {
+            return margin + xScale(nodes[d.head_id - 1].x);
+        })
+        .attr("y1", function(d) {
+            return margin + yScale(nodes[d.head_id - 1].y);
+        })
+        .attr("x2", function(d) {
+            return margin + xScale(nodes[d.tail_id - 1].x);
+        })
+        .attr("y2", function(d) {
+            return margin + yScale(nodes[d.tail_id - 1].y);
+        })
+        .attr("stroke", function(d) {
+            return colorEdgeType(d);
+        })
+        .attr("stroke-width", 4)
+        .attr("stroke-opacity", 0.5)
+        .on("mouseover", function() {
+            d3.select(this).attr("stroke-opacity", 1)
+        })
+        .on("mouseout", function() {
+            d3.select(this).attr("stroke-opacity", 0.5)
+        })
+
+
+    $('#graph-canvas line').tipsy({
+        gravity: 'w',
+        html: true,
+        title: function() {
+            var d = this.__data__;
+            var info = '';
+            info += 'edge_id: ' + d.edge_id + '<br />';
+            info += 'length: ' + d.length.toFixed(2) + '<br />';
+            info += 'radius: ' + (d.diameter / 2).toFixed(2) + '<br />';
+            info += 'flow: ' + d.flow.toFixed(2) + '<br />';
+            var pressureDiff = Math.abs(nodes[d.head_id - 1].pressure - nodes[d.tail_id - 1].pressure);
+            info += 'Delta: ' + pressureDiff.toFixed(2);
+            return info;
+        }
+    });
+}
+
+function drawGraph() {
+    var scale = getPositionScale(nodes);
+    var color = getPressureScale(nodes);
+    drawEdges(scale.x, scale.y);
+    drawNodes(scale.x, scale.y, color);
+}
+
+function render(error, node_info, edge_info, summary) {
+    if (error) {
+        console.log(error);
+    }
+    nodes = node_info.json_list.map(function(d) {
+        return {
+            node_id: +d.node_id,
+            node_name: d.node_name,
+            demand: +d.demand,
+            head: +d.head,
+            node_type: +d.node_type,
+            pressure: +d.head,
+            x: +d.x,
+            y: +d.y
+        };
+    });
+
+    edges = edge_info.json_list.map(function(d) {
+        return {
+            edge_id: +d.edge_id,
+            head_id: +d.head_id,
+            tail_id: +d.tail_id,
+            length: +d.length,
+            diameter: +d.diameter,
+            roughness: +d.roughness,
+            edge_type: +d.edge_type,
+            flow: 0.00
+        };
+    })
+
+    drawGraph();		    
+}
+
+function initialize(name) {
+    state = name;
+    d3.select("#graph-canvas").selectAll("*").remove();
+    var url_nodes = "/api/nodes/" + state;
+    var url_edges = "/api/edges/" + state;		    
+    d3.queue()
+        .defer(d3.json, url_nodes)
+        .defer(d3.json, url_edges)		        
+        .await(render);
+}
+
+function predirection() {
+    var url_direction = "/api/predirection/" + state;
+    d3.json(url_direction, function(info) {
+        // console.log(edges[0]);
+        edges = _.merge(edges, info.json_list);
+        // console.log(edges[0]);
+        // showDirection(info.json_list);
+    });
+}
+
+function showDirection() {
+
+    var tempFlow = d3.select("#graph-canvas").selectAll("temp-circle")
+        .data(edges).enter().append("circle")
+        .moveToBack()
+        .attr("r", 2)
+        .attr("cx", function(d) {                        
+            return margin + xScale(nodes[d.head_id - 1].x);
+        })
+        .attr("cy", function(d) {
+            return margin + yScale(nodes[d.head_id - 1].y);
+        })                
+        .attr("fill", "steelblue")
+        .transition().duration(2000)        
+        .attr("cx", function(d) {
+            return margin + xScale(nodes[d.tail_id - 1].x);
+        })
+        .attr("cy", function(d) {
+            return margin + yScale(nodes[d.tail_id - 1].y);
+        });
+        
+
+    tempFlow.remove();
+}
+
+function update(error, info) {
+    info.edges.forEach(function(d) {
+        d.flow = d.flow * 1000
+    })
+    nodes = _.merge(nodes, info.nodes);
+    edges = _.merge(edges, info.edges);
+
+    pressureScale = getPressureScale();
+    flowScale = getFlowScale();
+
+    d3.select("#graph-canvas").selectAll("line")
+        .transition().duration(2000)
+        .attr("stroke-width", function(d) {
+            return flowScale(d.flow);
+        });
+
+    d3.select("#graph-canvas").selectAll("circle").filter(function(d, i) {
+            return d.node_type == JUNCTION;
+        })
+        .transition().duration(2000)
+        .attr("fill", function(d) {
+            return pressureScale(d.pressure);
+        });
+
+    d3.select("#graph-canvas").selectAll("circle").filter(function(d, i) {
+            return d.node_type == CONSUMER;
+        })
+        .transition().duration(2000)
+        .attr("fill", function(d) {
+            if (d.d_satisfy && d.h_satisfy) {
+                return "#F6416C";
+            }
+            return "white"
+        });
+
+    updateTable();
+
+}
+
+function update_node(error, info) {
+    nodes = _.merge(nodes, info.nodes);
+
+    d3.select("#graph-canvas").selectAll("circle").filter(function(d, i) {
+            return d.node_type == JUNCTION;
+        })
+        .transition().duration(500)
+        .attr("r", 5)
+        .transition().duration(500)
+        .attr("r", 2);
+
+    d3.select("#graph-canvas").selectAll("circle").filter(function(d, i) {
+            return d.node_type == CONSUMER;
+        })
+        .transition().duration(2000)
+        .attr("fill", function(d) {
+            if (d.d_satisfy && d.h_satisfy) {
+                return "#F6416C";
+            }
+            return "white"
+        });
+}
+
+function update_edge(error, info) {
+    info.edges.forEach(function(d) {
+        d.flow = d.flow * 1000
+    })
+    edges = _.merge(edges, info.edges);
+
+    d3.select("#graph-canvas").selectAll("line")
+        .transition().duration(500)
+        .attr("stroke-width", function(d) {
+            return flowScale(d.flow) + 2;
+        })
+        .transition().duration(500)
+        .attr("stroke-width", function(d) {
+            return flowScale(d.flow);
+        });
+}
+
+function updateData() {
+    var url = "/api/imaginary/" + state;
+    d3.queue()
+        .defer(d3.json, url)
+        .await(update);
+}
+
+function updateData1() {
+    var url = "/api/imaginary_pressure/" + state;
+    d3.queue()
+        .defer(d3.json, url)
+        .await(update_node);
+}
+
+function updateData2() {
+    var url = "/api/max_flow/" + state;
+    d3.queue()
+        .defer(d3.json, url)
+        .await(update_edge);
+}
+
+function updateData3() {
+    var iter = document.getElementById('iteration').value;
+    if (!iter) {
+        iter = 1;
+    }
+    var url = "/api/iter/" + state + "/" + iter;        
+    console.log(url)
+    d3.queue()
+        .defer(d3.json, url)
+        .await(test);
+}
--- static/write-table.js
+++ static/write-table.js
@@ -1,1 +1,394 @@
+$(window).load(function () {	
+	initialize(state);
+	getSummary();	
+	if (loop) {
+		flowLoop = setInterval(function() {
+       		showDirection();
+    	}, 2000);			
+	} 
+
+	w3.includeHTML();
+});
+
+function updateTable() {
+	getCustomers();
+	getSources();
+	getValves();
+	getKeyNodes();
+	getKeyEdges();
+}
+
+function loopAnimation() {
+	loop = !loop;
+	if (loop) {
+		$("#direction-button")
+		.attr('class', 'btn btn-danger')
+		.attr('value', 'Pause');
+		flowLoop = setInterval(function() {
+       		showDirection();
+    	}, 2000);			
+	} else {
+		$("#direction-button")
+		.attr('class', 'btn btn-default')
+		.attr('value', 'Play');
+		clearInterval(flowLoop)
+	}
+
+}
+ 
+function getSummary() {
+	var url_summary = "/api/summary/" + state;
+	d3.json(url_summary, writeSummaryTable);
+}
+
+function getCustomers() {
+	var url_customers = "/api/customers_table_info/" + state;
+	d3.json(url_customers, writeCustomersTable);
+}
+
+function getSources() {
+	var url_sources = "/api/sources_table_info/" + state;
+	d3.json(url_sources, writeSourcesTable);
+}
+
+function getValves() {
+	var url_valves = "/api/valves_table_info/" + state;
+	d3.json(url_valves, writeValvesTable);
+}
+
+function getValvesDynamic() {
+	var url_valves = "/api/valves_table_info/" + state;
+	d3.json(url_valves, writeValvesDynamic);
+}
+
+function writeValvesDynamic(edges) {
+	json_data = edges.valves_edges_list;
+	create_dynamic_table('#qq', json_data, ['edge_id', 'head_id', 'tail_id', 'valve_status']);
+}
+
+function getSourcesDynamic(){
+	var url_sources = "/api/sources_table_info/" + state;
+	d3.json(url_sources, writeSourcesDynamic);
+}
+function writeSourcesDynamic(nodes) {
+	json_data = nodes.sources_nodes_list;
+	create_dynamic_table('#qq', json_data, ['node_id', 'flow_out', 'pressure', 'min_pressure']);
+}
+
+function getCustomersDynamic() {
+	var url_customers = "/api/customers_table_info/" + state;
+	d3.json(url_customers, writeCustomersDynamic);
+
+}
+
+function writeCustomersDynamic(nodes) {
+	json_data = nodes.customers_nodes_list;
+	create_dynamic_table('#qq', json_data, ['node_id', 'demand', 'flow_in', 'flow_satisfied', 'pressure', 'min_pressure', 'pressure_satisfied']);
+}
+
+function getKeyNodes() {
+	document.getElementById("top-five-flow").click();
+}
+
+function getKeyEdges() {
+	document.getElementById("top-five-pressure").click();
+}
+
+function get_top_five_highest_pressure_nodes() {
+	var url_top_five_pressure_nodes = "/api/five_highest_pressure/" + state;
+	d3.json(url_top_five_pressure_nodes, writeTopFivePressureTable);
+	
+}
+
+function get_top_five_lowest_pressure_nodes() {
+	var url_top_five_pressure_nodes = "/api/five_lowest_pressure/" + state;
+	d3.json(url_top_five_pressure_nodes, writeTopFivePressureTable);
+	
+}
+
+
+function get_top_five_highest_flow_edges() {
+	var url_top_five_flow_edges = "/api/five_highest_flow/" + state;
+	d3.json(url_top_five_flow_edges, writeTopFiveFlowTable);
+	
+}
+
+function get_top_five_lowest_flow_edges() {
+	var url_top_five_flow_edges= "/api/five_lowest_flow/" + state;
+	d3.json(url_top_five_flow_edges, writeTopFiveFlowTable);
+	
+}
+function get_specified_node() {
+	var node_id = document.getElementById('search_node_id').value
+	var url_specified_node= "/api/nodes/" + state + "/" + node_id;
+	d3.json(url_specified_node, writeTopFivePressureTable);
+}
+
+
+function get_specified_edge() {
+	var edge_id = document.getElementById('search_edge_id').value
+	var url_specified_edge= "/api/edges/" + state + "/" + edge_id;
+	d3.json(url_specified_edge, writeTopFiveFlowTable);
+}
+
+function writeSummaryTable(summary) {
+	d3.select("#td-name").text(summary.name);
+    d3.select("#td-num_node").text(summary.num_node);
+    d3.select("#td-num_edge").text(summary.num_edge);
+    d3.select("#td-num_customer").text(summary.num_customer);
+    d3.select("#td-num_source").text(summary.num_source);
+    d3.select("#td-num_tank").text(summary.num_tank);
+    d3.select("#td-num_pump").text(summary.num_pump);
+    d3.select("#td-num_valve").text(summary.num_valve);
+}
+
+function writeCustomersTable(nodes) {
+	console.log(nodes)
+	var customers_nodes_list = nodes.customers_nodes_list;
+
+	for (i = 1; i <= 5; i++) {
+		d3.select("#customers-name" + i).text("");
+    	d3.select("#customers-id" + i).text("");
+    	d3.select("#customers-demand" + i).text("");
+    	d3.select("#customers-flow_in" + i).text("");
+	    d3.select("#customers-flow_satisfied" + i).selectAll("span").remove();
+	    d3.select("#customers-pressure" + i).text("");
+	    d3.select("#customers-min_pressure" + i).text("");
+	    d3.select("#customers-pressure_satisfied" + i).selectAll("span").remove();
+	}
+
+	for (i = 1; i <= customers_nodes_list.length; i++) {
+		d3.select("#customers-name" + i).text(customers_nodes_list[i-1].name);
+    	d3.select("#customers-id" + i).text(customers_nodes_list[i-1].node_id);
+    	d3.select("#customers-demand" + i).text(customers_nodes_list[i-1].demand);
+    	d3.select("#customers-flow_in" + i).text(parseFloat(customers_nodes_list[i-1].flow_in).toFixed(2));
+
+	    if (customers_nodes_list[i-1].flow_satisfied) {
+	    	d3.select("#customers-flow_satisfied" + i).append("xhtml:span")
+		    .attr("class", "control glyphicon glyphicon-ok")
+		    .attr("style", "color:#5cb85c");
+	    } else {
+	    	d3.select("#customers-flow_satisfied" + i).append("xhtml:span")
+		    .attr("class", "control glyphicon glyphicon-remove")
+		    .attr("style", "color:#d9534f");
+	    }
+
+	    d3.select("#customers-pressure" + i).text(parseFloat(customers_nodes_list[i-1].pressure).toFixed(2));
+	    d3.select("#customers-min_pressure" + i).text(customers_nodes_list[i-1].min_pressure);
+
+	    if (customers_nodes_list[i-1].pressure_satisfied) {
+	    	d3.select("#customers-pressure_satisfied" + i)
+	    	.append("xhtml:span")
+		    .attr("class", "control glyphicon glyphicon-ok")
+		    .attr("style", "color:#5cb85c");
+	    } else {
+	    	d3.select("#customers-pressure_satisfied" + i).append("xhtml:span")
+		    .attr("class", "control glyphicon glyphicon-remove")
+		    .attr("style", "color:#d9534f");
+	    }
+	}
+}
+
+
+function writeSourcesTable(nodes) {
+	console.log(nodes)
+	var sources_nodes_list = nodes.sources_nodes_list;
+
+	for (i = 1; i <= 5; i++) {
+		d3.select("#sources-id" + i).text("");
+	    d3.select("#sources-flow_out" + i).text("");
+	    d3.select("#sources-pressure" + i).text("");
+	    d3.select("#sources-min_pressure" + i).text("");
+	    d3.select("#sources-pressure_satisfied" + i).selectAll("span").remove();
+	}
+
+	for (i = 1; i <= 5; i++) {
+		d3.select("#sources-id" + i).text(sources_nodes_list[i-1].node_id);
+	    d3.select("#sources-flow_out" + i).text(parseFloat(sources_nodes_list[i-1].flow_out).toFixed(2));
+	    d3.select("#sources-pressure" + i).text(parseFloat(sources_nodes_list[i-1].pressure).toFixed(2));
+	    d3.select("#sources-min_pressure" + i).text(sources_nodes_list[i-1].min_pressure);
+
+	    if (sources_nodes_list[i-1].pressure >= sources_nodes_list[i-1].min_pressure) {
+	    	d3.select("#sources-pressure_satisfied" + i).append("xhtml:span")
+		    .attr("class", "control glyphicon glyphicon-ok")
+		    .attr("style", "color:#5cb85c");
+	    } else {
+	    	d3.select("#sources-pressure_satisfied" + i).append("xhtml:span")
+		    .attr("class", "control glyphicon glyphicon-remove")
+		    .attr("style", "color:#d9534f");
+	    }
+	}
+}
+
+function writeValvesTable(valves) {
+	console.log(valves)
+	var valves_edges_list = valves.valves_edges_list;
+
+	for (i = 1; i <= 5; i++) {
+		d3.select("#valves-edge_id" + i).text("");
+	    d3.select("#valves-head_id" + i).text("");
+	    d3.select("#valves-tail_id" + i).text("");
+	    d3.select("#valves-valve_flow" + i).text("");
+	    d3.select("#valves-valve_status" + i).selectAll("span").remove();
+	}
+
+	for (i = 1; i <= 5; i++) {
+		d3.select("#valves-edge_id" + i).text(valves_edges_list[i-1].edge_id);
+	    d3.select("#valves-head_id" + i).text(valves_edges_list[i-1].head_id);
+	    d3.select("#valves-tail_id" + i).text(valves_edges_list[i-1].tail_id);
+	    d3.select("#valves-valve_flow" + i).text(parseFloat(valves_edges_list[i-1].valve_flow).toFixed(2));
+
+	    if (valves_edges_list[0].valve_status) {
+	    	d3.select("#valves-valve_status" + i).append("xhtml:span")
+		    .attr("class", "control glyphicon glyphicon-ok-circle")
+		    .attr("style", "color:#5cb85c");
+	    } else {
+	    	d3.select("#valves-valve_status" + i).append("xhtml:span")
+		    .attr("class", "control glyphicon glyphicon-ban-circle")
+		    .attr("style", "color:#d9534f");
+	    }
+	}
+}
+
+
+function writeTopFivePressureTable(nodes){
+	console.log(nodes)
+	var top_five_pressure_nodes_list = nodes.json_list;
+
+	for (i = 1; i <= 5; i++) {
+		d3.select("#top_five_pressure-id" + i).text("");
+	    d3.select("#top_five_pressure-min_pressure" + i).text("");
+	    d3.select("#top_five_pressure-pressure" + i).text("");
+	    d3.select("#top_five_pressure-type" + i).selectAll("span").remove();
+	    d3.select("#top_five_pressure-satisfied" + i).selectAll("span").remove();
+	}
+
+	for (i = 1; i <= top_five_pressure_nodes_list.length; i++) {
+		d3.select("#top_five_pressure-id" + i).text(top_five_pressure_nodes_list[i-1].node_id);
+	    d3.select("#top_five_pressure-min_pressure" + i).text(parseFloat(top_five_pressure_nodes_list[i-1].head).toFixed(2));
+	    d3.select("#top_five_pressure-pressure" + i).text(parseFloat(top_five_pressure_nodes_list[i-1].pressure).toFixed(2));
+	    var node_type = top_five_pressure_nodes_list[i-1].node_type;
+	    switch (node_type) {
+	    	case CONSUMER:
+	            d3.select("#top_five_pressure-type" + i).append("xhtml:span")
+			    .attr("class", "label label-danger")
+			    .text("consumer");
+			    break;
+	        case SOURCE:
+	            d3.select("#top_five_pressure-type" + i).append("xhtml:span")
+			    .attr("class", "label label-warning")
+			    .text("source");
+			    break;
+	        case TANK:
+	            d3.select("#top_five_pressure-type" + i).append("xhtml:span")
+			    .attr("class", "label label-success")
+			    .text("tank");
+			    break;
+	        default:
+	            d3.select("#top_five_pressure-type" + i).append("xhtml:span")
+			    .attr("class", "label label-primary")
+			    .text("junction");
+	    }
+	    d3.select("#top_five_pressure-type" + i).text();	   
+	    if (top_five_pressure_nodes_list[i-1].pressure >= top_five_pressure_nodes_list[i-1].head) {	    		    	
+	    	d3.select("#top_five_pressure-satisfied" + i).append("xhtml:span")
+		    .attr("class", "control glyphicon glyphicon-ok")
+		    .attr("style", "color:#5cb85c");
+	    } else {	    		    
+	    	d3.select("#top_five_pressure-satisfied" + i).append("xhtml:span")
+		    .attr("class", "control glyphicon glyphicon-remove")
+		    .attr("style", "color:#d9534f");
+	    }
+	}
+}
+
+
+function writeTopFiveFlowTable(edges){
+	console.log(edges)
+	var top_five_flow_edges_list = edges.json_list;
+
+	for (i = 1; i <= 5; i++) {
+		d3.select("#top_five_flow-edge_id" + i).text("");
+	    d3.select("#top_five_flow-head_id" + i).text("");
+	    d3.select("#top_five_flow-tail_id" + i).text("");
+	    d3.select("#top_five_flow-flow" + i).text("");
+	    d3.select("#top_five_flow-type" + i).selectAll("span").remove();
+	    // d3.select("#top_five_pressure-satisfied" + i).selectAll("span").remove();
+	}
+
+	for (i = 1; i <= top_five_flow_edges_list.length; i++) {
+		d3.select("#top_five_flow-edge_id" + i).text(top_five_flow_edges_list[i-1].edge_id);
+	    d3.select("#top_five_flow-head_id" + i).text(top_five_flow_edges_list[i-1].head_id);
+	    d3.select("#top_five_flow-tail_id" + i).text(top_five_flow_edges_list[i-1].tail_id);
+	    d3.select("#top_five_flow-flow" + i).text(parseFloat(top_five_flow_edges_list[0].flow).toFixed(2));	   
+	    
+
+	    var edge_type = top_five_flow_edges_list[i-1].edge_type;
+	    switch (edge_type) {
+	    	case PUMP:
+	            d3.select("#top_five_flow-type" + i).append("xhtml:span")
+			    .attr("class", "label label-warning")
+			    .text("pump");
+			    break;		  
+	        case VALVE:
+	        	d3.select("#top_five_flow-type" + i).append("xhtml:span")
+			    .attr("class", "label label-success")
+			    .text("valve");	            
+			    break;
+	        default:
+	        	d3.select("#top_five_flow-type" + i).append("xhtml:span")
+			    .attr("class", "label label-primary")
+			    .text("pipe");	            
+	    }   
+	    // if (top_five_pressure_nodes_list[i-1].pressure >= top_five_pressure_nodes_list[i-1].head) {	    		    	
+	    // 	d3.select("#top_five_pressure-satisfied" + i).append("xhtml:span")
+		   //  .attr("class", "control glyphicon glyphicon-ok")
+		   //  .attr("style", "color:#5cb85c");
+	    // } else {	    		    
+	    // 	d3.select("#top_five_pressure-satisfied" + i).append("xhtml:span")
+		   //  .attr("class", "control glyphicon glyphicon-remove")
+		   //  .attr("style", "color:#d9534f");
+	    // }
+	}
+
+}
+
+function print_test() {
+	console.log("testtest");
+}
+
+
+
+function create_dynamic_table(label_identifier,data, columns) {
+	var table = d3.select(label_identifier).append('table')
+	var thead = table.append('thead')
+	var	tbody = table.append('tbody');
+	
+	thead.append('tr')
+		.selectAll('th')		
+		.data(columns).enter()
+		.append('th')	
+		.text(function (column) { return column; });
+		
+
+		// create a row for each object in the data
+	var rows = tbody.selectAll('tr')
+		.data(data)
+		.enter()
+		.append('tr');
+
+
+		// create a cell in each row for each column
+	var cells = rows.selectAll('td')
+		.data(function (row) {
+		    return columns.map(function (column) {
+		      return {column: column, value: row[column]};
+		    });
+		  })
+		  .enter()
+		  .append('td')		  
+		    .text(function (d) { return d.value; });
+
+	return table;
+}
 
